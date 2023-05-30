3803d7d3e0e7794fdef8fdb9aa0f57b44afe92b3
AbiollaghJames <abiollajames@protonmail.com>
2023-05-30 02:16:47 -0700

    is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,12 +9,12 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 	{
-		return 0;
+		return(0);
 	}
 	else
 	{
 		if (node->left == NULL && node->right == NULL)
-			return 1;
-		return 0;
+			return (1);
+		return (0);
 	}
 }
